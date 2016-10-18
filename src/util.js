350f2f13102f4acbaa89a71212b43aedb606d238
maumasi <liu.maumasi@gmail.com>
2016-10-17 22:46:33 -0400

    made some tests for new log tool edits

--- src/util.js
+++ src/util.js
@@ -1,8 +1,6 @@
 // time stamp
 const stamp = new Date();
 
-const fs = require('fs');
-
 const seperator = '======' + stamp + '=======';
 
 const colors = require('colors');
@@ -10,28 +8,23 @@ const colors = require('colors');
 // utility method of debug
 exports.debug = (data, status) => {
   // debug env
+  let state;
   if (process.env.DEBUG) {
     if (status === 'fail') {
       const out1 = seperator + '\n \n' + colors.red(data) + '\n \n';
-      fs.appendFile('./error.log', out1, (err) => {
-        if (err) throw err;
-        // closing fs appendfile
-      });
-      console.log(out1);
+      state = status;
+      console.error(out1);
     } else if (status === 'sucess') {
       const out2 = seperator + '\n \n' + colors.green(data) + '\n \n';
-      fs.appendFile('./error.log', out2, (err) => {
-        if (err) throw err;
-      }); // closing fs appendfile
+      state = status;
       console.log(out2);
       // if status is undefined
     } else {
       const out3 = seperator + '\n \n' + colors.magenta(data) + '\n \n';
-      fs.appendFile('./error.log', out3, (err) => {
-        if (err) throw err;
-      }); // closing fs appendfile
-      console.log(out3);
+      console.warn(out3);
+      state = 'warn';
     } // closes else
     console.log('log updated sucessfully');
+    return state;
   } // closes if debug env
 }; // closes util module

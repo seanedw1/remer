d9ec87fd8bdd41721386b176bf3dc6c0ea71398f
Washeen <washeen91@fullsail.edu>
2016-10-19 22:28:47 -0400

    Version Bumper Modified
    Modified Version Bumper

--- src/version_updater/vupdate.js
+++ src/version_updater/vupdate.js
@@ -1,18 +1,32 @@
 const packageVersion = require('../../package.json');
 
 
-
 // Current Version
-const versionNow = packageVersion.version;
+const versionNow = packageVersion.version.split('.');
+
+// Capturing the arrays in to const, splitted by the previous code in line 5
+const prevMajor = parseInt(versionNow[0], 10);
+const prevMinor = parseInt(versionNow[1], 10);
+const prevPatch = parseInt(versionNow[2], 10);
 
 
-console.log('Major' + versionNow.charAt(0));
-console.log('Minor' + versionNow.charAt(2));
-console.log('Patch' + versionNow.charAt(4));
 
-// const majorOld = versionNow.charAt(0);
-// const minorOld = versionNow.charAt(2);
-// const patchOld = versionNow.charAt(4);
+exports.up = (msg, updater) => {
+  // If it is major then version will add 1 on major
+  if (updater === 'major') {
+    const newmajor = prevMajor + 1;
+    console.log(newmajor + '.' + prevMinor + '.' + prevPatch);
 
+    // If it is minor then version will add 1 on minor
+  } else if (updater === 'minor') {
+    const newminor = prevMinor + 1;
+    console.log(prevMajor + '.' + newminor + '.' + prevPatch);
 
-// var majorOld = new RegExp("\d.");
+    // If it is patch then version will add 1 on patch
+  } else if (updater === 'patch') {
+    const newpatch = prevMinor + 1;
+    console.log(prevMajor + '.' + prevMinor + '.' + newpatch);
+  } else {
+    console.log('Your Current Package', packageVersion.version)
+  }
+};
